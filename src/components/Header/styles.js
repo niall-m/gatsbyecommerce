b8b191dccaa759ffe657812233224ed97ba9dc39
Niall Mahford <niall.mahford@gmail.com>
2021-02-23 10:07:39 -0800

    Add Header, Cart to Layout

---
+++ src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.header`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 10px 20px;
+  box-sizing: border-box;
+
+  display: flex;
+  flex-wrap: wrap;
+`;
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ export * from './SEO';
 export * from './Layout';
 export * from './ImageGallery';
 export * from './ProductQuantityAdder';
+export * from './Header';
+export * from './Cart';
 export * from './Input';
 export * from './Button';
