3f731399ec34ea779f07c6bcc219cfd1b21ec605
Niall Mahford <niall.mahford@gmail.com>
2021-02-23 11:50:18 -0800

    convert Link to styled component

--- src/components/Cart/styles.js
+++ src/components/Cart/styles.js
@@ -1,12 +1,21 @@
 import styled from 'styled-components';
+import { StyledLink } from '../StyledLink';
 
-export const CartWrapper = styled.div`
+export const CartWrapper = styled(StyledLink).attrs(() => ({
+  to: '/cart'
+}))`
   margin-left: auto;
   display: flex;
   align-items: center;
+  color: black;
+  text-decoration: none;
 
   > div:last-child {
     padding-left: 8px;
     margin: auto 0;
+  }
+
+  &:hover {
+    text-decoration: underline;
   }
 `;
