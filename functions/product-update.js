56498797573763dcaa7e0e510cb1df806412a0be
Niall Mahford <niall.mahford@gmail.com>
2021-02-24 14:02:49 -0800

    product update function

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,6 +1,7 @@
 // running in netlify cloud function node environment
 const faunadb = require('faunadb');
 const verifyWebhookIntegrity = require('shopify-verify-webhook');
+const axios = require('axios');
 
 const q = faunadb.query;
 const client = new faunadb.Client({
@@ -12,10 +13,46 @@ exports.handler = function(event, context, callback) {
     event.headers['x-shopify-hmac-sha256'], event.body);
   
   if (isValid) {
-    callback(null, {
-      statusCode: 200,
-      body: 'Hello, World!',
+    const body = JSON.parse(event.body);
+    const { id } = body; // updated product
+    // remove updated date/inventory
+    delete body.updated_at;
+    body.variants.forEach(variant => {
+      delete variant.updated_at;
+      delete variant.inventory_quantity;
+      delete variant.old_inventory_quantity;
     });
+
+    const bodyString = JSON.stringify(body);
+
+    client
+      .query(q.Get(q.Match(q.Index('product_by_id'), id)))
+      .then(result => { // query succeeded, diff fauna and shopify hook payload
+        if (result.data.product !== bodyString) {
+          // update product in fauna, ref product doc, not product id
+          client.query(q.Update(result.ref, {
+            data: { product: bodyString }
+          }))
+          .then(() => { // call rebuild
+            axios.post(process.env.NETLIFY_BUILD_URL);
+          })
+          .catch(e => {
+            console.log('error updating product: ', e);
+          });
+        }
+      })
+      .catch(() => { // query failed, product id not found
+        client // add product to db
+          .query(q.Create(q.Collection('products'), { 
+            data: { id, product: bodyString }
+          }))
+          .then(() => { // call rebuild
+            axios.post(process.env.NETLIFY_BUILD_URL);
+          })
+          .catch(e => {
+            console.log('error adding to db: ', e);
+          });
+      });
   } else {
     callback(null, {
       statusCode: 403,
