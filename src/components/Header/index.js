8529e55d377b80114d8e8f8c8d5891a532254cb6
Niall Mahford <niall.mahford@gmail.com>
2021-02-24 10:25:41 -0800

    linked Logo, local query

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { HeaderWrapper } from './styles';
 import { Cart } from '../Cart';
 import { Search } from '../Search';
+import { Logo } from '../Logo';
+import { Link } from 'gatsby';
 
 export function Header() {
   return (
     <HeaderWrapper>
+      <div>
+        <Link to={"/"}>
+          <Logo />
+        </Link>
+      </div>
       <Search />
       <Cart />
     </HeaderWrapper>
