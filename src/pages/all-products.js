d3211992a1bbea40bdacdacaa9c23c220333e8e3
Niall Mahford <niall.mahford@gmail.com>
2021-02-23 17:53:25 -0800

    rendering filtered products

--- src/pages/all-products.js
+++ src/pages/all-products.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Layout, Filters } from 'components';
+import { Layout, Filters, ProductsGrid } from 'components';
 import ProductContext from 'context/ProductContext';
 import styled from 'styled-components';
+import queryString from 'query-string';
+import { useLocation } from '@reach/router';
 
 const Content = styled.div`
   display: grid;
@@ -12,14 +14,55 @@ const Content = styled.div`
 
 export default function AllProducts() {
   const { products, collections } = React.useContext(ProductContext);
-  console.log('products', products);
+  const collectionProductMap = {};
+  const { search } = useLocation();
+  const qs = queryString.parse(search);
+  const selectedCollectionIds = qs.c?.split(',').filter(c => Boolean(c)) || [];
+  // get collectionIds in URI, remove empty strings, default to array
+  const selectedCollectionIdsMap = {};
+
+  selectedCollectionIds.forEach(collectionId => {
+    selectedCollectionIdsMap[collectionId] = true;
+  });
+
+  if (collections) {
+    collections.forEach(collection => {
+      collectionProductMap[collection.shopifyId] = {};
+
+      collection.products.forEach(product => {
+        collectionProductMap[collection.shopifyId][product.shopifyId] = true;
+      });
+    });
+  }
+
+  console.log("collectionProductMap", collectionProductMap);
+  
+  const filterByCategory = (product) => {
+    // return whether product matches category
+    if (Object.keys(selectedCollectionIdsMap).length) {
+      // if it has keys, loop through
+      for (let key in selectedCollectionIdsMap) {
+        if (collectionProductMap[key]?.[product.shopifyId]) {
+          // if key in map and product belongs to collection
+          return true;
+        }
+      }
+      return false;
+    }
+    // no selected categories, return each
+    return true;
+  };
+  
+  const filteredProducts = products.filter(filterByCategory);
   
   return (
     <Layout>
-      <h4>{products.length} products</h4>
+      <h4>{filteredProducts.length} products</h4>
       <Content>
         <Filters />
-        <div>Products</div>
+        <div>
+          <ProductsGrid products={filteredProducts} />
+        </div>
       </Content>
     </Layout>
   );
