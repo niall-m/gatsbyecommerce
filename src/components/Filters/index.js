dae20dc0ca05c787f3b83c7ad3ecb296cd068137
Niall Mahford <niall.mahford@gmail.com>
2021-02-23 17:07:25 -0800

    AllProducts Filters and Checkbox

---
+++ src/components/Filters/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ProductContext from 'context/ProductContext';
+import { CategoryFilterItem } from './CategoryFilterItem';
+import { FiltersWrapper } from './styles';
+
+export function Filters() {
+  const { collections } = React.useContext(ProductContext);
+
+  return (
+    <FiltersWrapper>
+      <strong>Categories</strong>
+      {collections.map(collection => (
+        <CategoryFilterItem key={collection.shopifyId} title={collection.title} />
+      ))}
+    </FiltersWrapper>
+  );
+}
--- src/pages/all-products.js
+++ src/pages/all-products.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { Layout } from 'components';
+import { Layout, Filters } from 'components';
 import ProductContext from 'context/ProductContext';
+import styled from 'styled-components';
+
+const Content = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  margin-top: 20px;
+  grid-template-columns: 1fr 3fr;
+`;
 
 export default function AllProducts() {
   const { products, collections } = React.useContext(ProductContext);
@@ -9,11 +17,10 @@ export default function AllProducts() {
   return (
     <Layout>
       <h4>{products.length} products</h4>
-      <div>
-        {collections.map(collection => (
-          <div key={collection.shopifyId}>{collection.title}</div>
-        ))}
-      </div>
+      <Content>
+        <Filters />
+        <div>Products</div>
+      </Content>
     </Layout>
   );
 }
